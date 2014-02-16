6fb67b14316cfa22288f0afaf287a6c2847d7a8c
Bohan <bohanhsu1990@gmail.com>
2014-02-15 20:39:21 -0500

    2/14

---
+++ workspace/alg_hw4/src/excel/Table.java
@@ -0,0 +1,12 @@
+package excel;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Table {
+	ArrayList<HashMap<Integer, Double>> distArray = new ArrayList<>()
+	
+//	HashMap<Integer, Double> dist = new HashMap<Integer, Double>();
+//	HashMap<Integer, Integer> prev = new HashMap<Integer, Integer>();
+
+}
