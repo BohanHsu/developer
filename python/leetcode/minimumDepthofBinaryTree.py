cb08e900b768332058506fe71add9cac44262c03
Bohan Xu <xu.bo@ic685b3593c123.nunet.neu.edu>
2014-07-19 15:24:54 -0400

    7.19

--- python/leetcode/minimumDepthofBinaryTree.py
+++ python/leetcode/minimumDepthofBinaryTree.py
@@ -9,7 +9,7 @@ class Solution:
   # @param root, a tree node
   # @return an integer
   def minDepth(self, root):
-    queue =  collection.deque()
+    queue =  collections.deque()
     depth = {}
     depth[root] = 1
     queue.append(root)
@@ -26,3 +26,19 @@ class Solution:
         queue.append(n.right)
         depth[n.right] = depth[n] + 1
 
+# test code 
+t1 = TreeNode(1)
+t2 = TreeNode(2)
+t3 = TreeNode(3)
+t4 = TreeNode(4)
+t5 = TreeNode(5)
+t6 = TreeNode(6)
+
+t1.left = t2
+t1.right = t3
+t2.left = t4
+t2.right = t5
+#t3.left = t6
+
+s = Solution()
+print s.minDepth(t1)
