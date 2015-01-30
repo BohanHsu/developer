80557d000dc4ac3c3a9ed3352826d09d6a64868c
Bohan <bohanhsu1990@gmail.com>
2015-01-30 09:32:22 -0500

    1/30

---
+++ neu/cs6220/hw1/src/plot36.py
@@ -0,0 +1,30 @@
+import plotly.plotly as py
+from plotly.graph_objs import *
+
+f = open('./../results/problem1.txt')
+lines = f.readlines()
+
+ls1 = [s[:-1].split(',') for s in lines]
+
+training_set_name = '150(1000)_100_train.csv'
+
+lambdas = []
+mses = []
+
+for line in ls1:
+    if line[0].strip() == training_set_name:
+        lambdas.append(int(line[1]))
+        mses.append(float(line[2]))
+
+print lambdas
+print mses
+
+trace1 = Scatter(
+        x = lambdas,
+        y = mses,
+        name = 'mses',
+        mode = 'lines+markers'
+        )
+
+data = Data([trace1])
+plot_url = py.plot(data, filename = training_set_name + '_cross_validation')
